3432126377e020375ed8c7710283e12ab3119174
dafi <akhadafi@kmklabs.com>
2016-09-22 19:50:12 +0700

    add view all stories

--- resources/views/crawler/index.blade.php
+++ resources/views/crawler/index.blade.php
@@ -3,25 +3,146 @@
 @section('content')
 <div class="container">
 
-    <h1>Crawler</h1>
-    <div class="table">
-        <table class="table table-bordered table-striped table-hover">
-            <thead>
-                <tr>
-                    <th> No </th>
-                    <th> Story Id </th>
-                    <th> Project Name </th>
-                    <th> Title </th>
-                    <th> Story Type </th>
-                    <th> Point </th>
-                </tr>
-            </thead>
-            <tbody>
-            @foreach($stories as $item)
-            @endforeach
-            </tbody>
-        </table>
+<div>
+  <!-- Nav tabs -->
+  <ul class="nav nav-tabs" role="tablist">
+    <li role="presentation" class="active">
+      <a href="#publishing" aria-controls="publishing" role="tab" data-toggle="tab">Publishing</a>
+    </li>
+    <li role="presentation">
+      <a href="#vidio" aria-controls="vidio" role="tab" data-toggle="tab">Vidio</a>
+    </li>
+    <li role="presentation">
+      <a href="#playback" aria-controls="playback" role="tab" data-toggle="tab">Playback</a>
+    </li>
+    <li role="presentation">
+      <a href="#apps" aria-controls="apps" role="tab" data-toggle="tab">Apps</a>
+    </li>
+  </ul>
+
+  <!-- Tab panes -->
+  <div class="tab-content">
+    <div role="tabpanel" class="tab-pane active" id="publishing">
+        <div class="container">
+        <h1>Get new story</h1>
+        <div class="table">
+            <table class="table table-bordered table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th> Story Id </th>
+                        <th> Title </th>
+                        <th> Project Name </th>
+                        <th> Story type & Points </th>
+                    </tr>
+                </thead>
+                <tbody>
+                @foreach($stories as $i => $item)
+                    <tr>
+                        <td>{{ $i+1 }}</td>
+                        <td>{{ "[#" . $item->pivotal_id . "]" }}</td>
+                        <td>{{ $item->title }}</td>
+                        <td>{{ $item->project_id }}</td>
+                        <td>{{ "(". $item->story_type . ", " . $item->point . ")"}}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+      </div>
     </div>
+    <div role="tabpanel" class="tab-pane" id="vidio">
+      <div class="container">
+        <h1>Get new story</h1>
+        <div class="table">
+            <table class="table table-bordered table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th> Story Id </th>
+                        <th> Title </th>
+                        <th> Project Name </th>
+                        <th> Story type & Points </th>
+                    </tr>
+                </thead>
+                <tbody>
+                @foreach($stories as $i => $item)
+                    <tr>
+                        <td>{{ $i+1 }}</td>
+                        <td>{{ "[#" . $item->pivotal_id . "]" }}</td>
+                        <td>{{ $item->title }}</td>
+                        <td>{{ $item->project_id }}</td>
+                        <td>{{ "(". $item->story_type . ", " . $item->point . ")"}}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+      </div>
+    </div>
+    </div>
+    <div role="tabpanel" class="tab-pane" id="playback">
+      <div class="container">
+        <h1>Get new story</h1>
+        <div class="table">
+            <table class="table table-bordered table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th> Story Id </th>
+                        <th> Title </th>
+                        <th> Project Name </th>
+                        <th> Story type & Points </th>
+                    </tr>
+                </thead>
+                <tbody>
+                @foreach($stories as $i => $item)
+                    <tr>
+                        <td>{{ $i+1 }}</td>
+                        <td>{{ "[#" . $item->pivotal_id . "]" }}</td>
+                        <td>{{ $item->title }}</td>
+                        <td>{{ $item->project_id }}</td>
+                        <td>{{ "(". $item->story_type . ", " . $item->point . ")"}}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+      </div>
+    </div>
+    </div>
+    <div role="tabpanel" class="tab-pane" id="apps">
+      <div class="container">
+        <h1>Get new story</h1>
+        <div class="table">
+            <table class="table table-bordered table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th> Story Id </th>
+                        <th> Title </th>
+                        <th> Project Name </th>
+                        <th> Story type & Points </th>
+                    </tr>
+                </thead>
+                <tbody>
+                @foreach($stories as $i => $item)
+                    <tr>
+                        <td>{{ $i+1 }}</td>
+                        <td>{{ "[#" . $item->pivotal_id . "]" }}</td>
+                        <td>{{ $item->title }}</td>
+                        <td>{{ $item->project_id }}</td>
+                        <td>{{ "(". $item->story_type . ", " . $item->point . ")"}}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+      </div>
+    </div>
+    </div>
+  </div>
+</div>
 
 </div>
 @endsection
