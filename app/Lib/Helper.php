8e3ee3daae26eeb8085477f06e8d25728cdf10b1
michimawan <blog.1393@gmail.com>
2016-10-26 23:55:45 +0700

    [ref #132900123] adding green tag parser and story grouping

--- app/Lib/Helper.php
+++ app/Lib/Helper.php
@@ -8,21 +8,77 @@ use Config;
 
 class Helper
 {
-	public function parse(string $textToParse = '') : array
+	public static function parseText(string $text) : array
 	{
-		$pattern = '/#(?P<id>\d+)\]/i';
+		$greenTags = self::parseGreenTag($text);
+		$greenTags = self::addStoriesToGreenTag($greenTags, $text);
+
+		$storyIds = self::parse($text);
+
+		return [$greenTags, $storyIds];
+	}
+
+	public static function parse(string $text = '') : array
+	{
+		$pattern = '/#(?P<story_id>\d+)\]/i';
 		$matches = [];
 		$ids = [];
 
-		$found = preg_match_all($pattern, $textToParse, $matches);
+		$found = preg_match_all($pattern, $text, $matches);
 		
 		if ($found) {
-			foreach($matches['id'] as $match) {
-				$ids[] = $match;
+			foreach($matches['story_id'] as $match) {
+				$ids[] = (int) $match;
 			}
 		}
 
 		return $ids;
+	}
+
+	// example match pattern
+	// #1587 (Oct 20, 2016 5:23:39 PM)
+	public static function parseGreenTag(string $text = '') : array
+	{
+		$pattern = '/(?P<green_tag_timing>(?P<green_tag_id>#(\d+)) \((?P<timing>[A-Za-z0-9,: ]+)\))/i';
+
+		$matches = [];
+		$greenTag = [];
+		$found = preg_match_all($pattern, $text, $matches);
+		if ($found) {
+			foreach($matches['green_tag_timing'] as $idx => $match) {
+				$greenTag[$match] = [
+					'greenTagId' => $matches['green_tag_id'][$idx],
+					'greenTagTiming' => $matches['timing'][$idx],
+				];
+			}
+		}
+
+		return $greenTag;
+	}
+
+	public static function addStoriesToGreenTag(array $greenTags = [], string $text = '') : array
+	{
+		$lines = preg_split("/\\r\\n|\\r|\\n/", $text);
+		$greenTagsString = array_keys($greenTags);
+
+		$lineCount = count($lines);
+		for ($i=0; $i < $lineCount ; $i++) { 
+			$stories = [];
+			if (in_array($lines[$i], $greenTagsString)) {
+				$j = $i + 1;
+				while( $j < $lineCount && ! in_array($lines[$j], $greenTagsString)) {
+					$ids = static::parse($lines[$j]);
+					if (count($ids)) {
+						$stories = array_merge($stories, $ids);
+					}
+					$j++;
+				}
+				$greenTags[$lines[$i]]['stories'] = $stories;
+				$i = $j - 1;
+			}
+		}
+
+		return $greenTags;
 	}
 
 	public function reverseProjectIds($groups = []) : array
