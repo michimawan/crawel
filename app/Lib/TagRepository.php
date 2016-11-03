fd859f39869ec967898d4c4f89d410bc7ea83a5c
michimawan <blog.1393@gmail.com>
2016-11-03 21:25:22 +0700

    [ref #132900123] tag repository add filter getByDate

--- app/Lib/TagRepository.php
+++ app/Lib/TagRepository.php
@@ -9,15 +9,15 @@ use App\Models\Tag;
 
 class TagRepository
 {
-	public function store($tags)
+	public function store($project, $tags)
 	{
         foreach ($tags as $greenTag) {
             $tag = new Tag;
             $tag->code = $greenTag['greenTagId'];
             $tag->timing = $greenTag['greenTagTiming'];
+            $tag->project = $project;
             $date = Carbon::today();
             $storedDate = Helper::sanitizeDate($date, ' ');
-            $tag->last_updated_at = json_encode([$storedDate]);
             try {
                 $tag->save();
             } catch(QueryException $e) {
@@ -27,7 +27,17 @@ class TagRepository
         }
 	}
 
-    public function getByDate($date = '')
+    public function getByDate($date = null)
     {
+        if ($date == null) {
+            $startDate = Carbon::today();
+            $endDate = Carbon::today()->addDay();
+        } else {
+            $date .= ' 00';
+            $startDate = Carbon::createFromFormat('Y-m-d H', $date);
+            $endDate = clone $startDate;
+            $endDate->addDay();
+        }
+        return Tag::where('created_at', '>=', $startDate)->where('created_at', '<=', $endDate)->get();
     }
 }
--- tests/Lib/TagRepositoryTest.php
+++ tests/Lib/TagRepositoryTest.php
@@ -34,7 +34,7 @@ class TagRepositoryTest extends BaseLibTest
     {
         $tagCount = Tag::count();
         $tagRepo = new TagRepository();
-        $tagRepo->store($this->data);
+        $tagRepo->store('foo', $this->data);
 
         $this->assertEquals($tagCount + 2, Tag::count());
         $this->assertEquals(5, Tag::where('code', '#1587')->first()->stories->count());
@@ -44,9 +44,9 @@ class TagRepositoryTest extends BaseLibTest
     public function test_store_same_greenTag_not_throw_exception_and_no_duplicating_row()
     {
         $tagRepo = new TagRepository();
-        $tagRepo->store($this->data);
+        $tagRepo->store('foo', $this->data);
         $tagCount = Tag::count();
-        $tagRepo->store($this->data);
+        $tagRepo->store('foo', $this->data);
         $this->assertEquals($tagCount, Tag::count());
     }
 
@@ -60,23 +60,41 @@ class TagRepositoryTest extends BaseLibTest
             ]
         ];
         $tagRepo = new TagRepository();
-        $tagRepo->store($data);
+        $tagRepo->store('foo', $data);
 
         $tag = Tag::where('code', '#1585')->first();
         $storyCount = $tag->stories->count();
 
         $data['#1585 (Oct 20, 2016 5:23:39 PM)']['stories'] = $this->stories2->pluck('pivotal_id')->all();
-        $tagRepo->store($data);
+        $tagRepo->store('foo', $data);
         $this->assertEquals(3, Tag::where('code', '#1585')->first()->stories->count());
     }
 
     public function test_getByDate_return_expected_greenTag()
     {
-        $tag = factory(Tag::class)->create();
+        $yesterday = Carbon::now()->subDay();
+        $tag = factory(Tag::class)->create([
+            'created_at' => $yesterday,
+        ]);
+        factory(Tag::class)->create();
         $tagRepo = new TagRepository();
+
+        $yesterdayDate = Helper::sanitizeDate(Carbon::today()->subDay()->toDateTimeString(), ' ');
+        $this->assertEquals(1, $tagRepo->getByDate($yesterdayDate)->count());
+        $this->assertEquals($tag->id, $tagRepo->getByDate($yesterdayDate)->first()->id);
     }
 
-    public function xtest_getByDate_when_no_date_send_return_today()
+    public function test_getByDate_when_no_date_send_return_today()
     {
+        $yesterday = Carbon::now()->subDay();
+        factory(Tag::class)->create([
+            'created_at' => $yesterday,
+        ]);
+        $tag = factory(Tag::class)->create();
+        $tagRepo = new TagRepository();
+
+        $yesterdayDate = Helper::sanitizeDate(Carbon::today()->subDay()->toDateTimeString(), ' ');
+        $this->assertEquals(1, $tagRepo->getByDate()->count());
+        $this->assertEquals($tag->id, $tagRepo->getByDate()->first()->id);
     }
 }
