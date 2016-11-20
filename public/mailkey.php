84d368be2a66ddc916e5cd2859b85fcd22a1609d
michimawan <blog.1393@gmail.com>
2016-11-21 06:02:57 +0700

    google example of creating draft

---
+++ public/mailkey.php
@@ -0,0 +1,91 @@
+
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+
+define('APPLICATION_NAME', 'Gmail API PHP Quickstart');
+define('CREDENTIALS_PATH', '~/.credentials/gmail-php-quickstart.json');
+define('CLIENT_SECRET_PATH', __DIR__ . '/../client_secret.json');
+// If modifying these scopes, delete your previously saved credentials
+// at ~/.credentials/gmail-php-quickstart.json
+define('SCOPES', implode(' ', [
+  Google_Service_Gmail::MAIL_GOOGLE_COM,
+  Google_Service_Gmail::GMAIL_MODIFY,
+  Google_Service_Gmail::GMAIL_COMPOSE,
+]));
+
+if (php_sapi_name() != 'cli') {
+  throw new Exception('This application must be run on the command line.');
+}
+
+/**
+ * Returns an authorized API client.
+ * @return Google_Client the authorized client object
+ */
+function getClient() {
+  $client = new Google_Client();
+  $client->setApplicationName(APPLICATION_NAME);
+  $client->setScopes(SCOPES);
+  $client->setAuthConfig(CLIENT_SECRET_PATH);
+  $client->setAccessType('offline');
+
+  // Load previously authorized credentials from a file.
+  $credentialsPath = expandHomeDirectory(CREDENTIALS_PATH);
+  if (file_exists($credentialsPath)) {
+    $accessToken = json_decode(file_get_contents($credentialsPath), true);
+  } else {
+    // Request authorization from the user.
+    $authUrl = $client->createAuthUrl();
+    printf("Open the following link in your browser:\n%s\n", $authUrl);
+    print 'Enter verification code: ';
+    $authCode = trim(fgets(STDIN));
+
+    // Exchange authorization code for an access token.
+    $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+
+    // Store the credentials to disk.
+    if(!file_exists(dirname($credentialsPath))) {
+      mkdir(dirname($credentialsPath), 0700, true);
+    }
+    file_put_contents($credentialsPath, json_encode($accessToken));
+    printf("Credentials saved to %s\n", $credentialsPath);
+  }
+  $client->setAccessToken($accessToken);
+
+  // Refresh the token if it's expired.
+  if ($client->isAccessTokenExpired()) {
+    $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+    file_put_contents($credentialsPath, json_encode($client->getAccessToken()));
+  }
+  return $client;
+}
+
+/**
+ * Expands the home directory alias '~' to the full path.
+ * @param string $path the path to expand.
+ * @return string the expanded path.
+ */
+function expandHomeDirectory($path) {
+  $homeDirectory = getenv('HOME');
+  if (empty($homeDirectory)) {
+    $homeDirectory = getenv('HOMEDRIVE') . getenv('HOMEPATH');
+  }
+  return str_replace('~', realpath($homeDirectory), $path);
+}
+
+// Get the API client and construct the service object.
+$client = getClient();
+$service = new Google_Service_Gmail($client);
+$message = new Google_Service_Gmail_Message();
+$email = strtr(base64_encode("Helo World"), array('+' => '-', '/' => '_'));
+print_r($email);
+$message->setRaw($email);
+$user = 'me';
+$draft = new Google_Service_Gmail_Draft();
+$draft->setMessage($message);
+try {
+  $draft = $service->users_drafts->create($user, $draft);
+  print 'Draft ID: ' . $draft->getId();
+} catch (Exception $e) {
+  print 'An error occurred: ' . $e->getMessage();
+}
